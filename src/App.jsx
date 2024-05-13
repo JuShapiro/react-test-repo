09e47b10135d3a4e285b9681a7fa882b0b97b782
JuShapiro <yuliashepel19@gmail.com>
2024-05-13 14:20:30 +0200

    firstPartPass

--- src/App.jsx
+++ src/App.jsx
@@ -65,21 +65,72 @@ import "./App.css";
 
 // export default App;
 
-const ClickCounter = () => {
-  const [clicks, setClicks] = useState(0);
+// const ClickCounter = () => {
+//   const [clicks, setClicks] = useState(0);
 
-  const handleClick = () => {
-    setClicks(clicks + 1);
-  };
-  return <button onClick={handleClick}>Current: {clicks}</button>;
-};
+//   const handleClick = () => {
+//     setClicks(clicks + 1);
+//   };
+//   return <button onClick={handleClick}>Current: {clicks}</button>;
+// };
+
+// const App = () => {
+//   return (
+//     <>
+//       <ClickCounter />
+//       <ClickCounter />
+//     </>
+//   );
+// };
+
+// export default App;
+
+// const ClickCounter = ({ value, onUpdate }) => {
+//   return <button onClick={onUpdate}>Current: {value}</button>;
+// };
+
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+
+//   const handleClick = () => {
+//     setClicks(clicks + 1);
+//   };
+//   return (
+//     <>
+//       <ClickCounter value={clicks} onUpdate={handleClick} />
+//       <ClickCounter value={clicks} onUpdate={handleClick} />
+//     </>
+//   );
+// };
+
+// export default App;
 
 const App = () => {
+  const [values, setValues] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const updateX = () => {
+    setValues({
+      ...values,
+      x: values.x + 1,
+    });
+  };
+  const updateY = () => {
+    setValues({
+      ...values,
+      y: values.y + 1,
+    });
+  };
   return (
-    <>
-      <ClickCounter />
-      <ClickCounter />
-    </>
+    <div>
+      <p>
+        x: {values.x} y: {values.y}
+      </p>
+      <button onClick={updateX}>UPDATE X</button>
+      <button onClick={updateY}>UPDATE Y</button>
+    </div>
   );
 };
 
