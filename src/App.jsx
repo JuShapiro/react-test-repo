587bcf8933e28c09020fd5c9c72ce1ac677197b4
JuShapiro <yuliashepel19@gmail.com>
2024-05-15 14:08:02 +0200

    addLastPart

--- src/App.jsx
+++ src/App.jsx
@@ -197,3 +197,70 @@ import "./App.css";
 // };
 
 // export default App;
+
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+//   useEffect(() => {
+//     console.log("You can see me only once");
+//   }, []);
+//   useEffect(() => {
+//     console.log("Clicks updated: ", clicks);
+//   }, [clicks]);
+//   useEffect(() => {
+//     document.title = `You clicked ${clicks} times`;
+//   });
+
+//   return (
+//     <button onClick={() => setClicks(clicks + 1)}>
+//       You clicked {clicks} times
+//     </button>
+//   );
+// };
+
+// export default App;
+
+// const App = () => {
+//   const [first, setFirst] = useState(0);
+//   const [second, setSecond] = useState(0);
+
+//   useEffect(() => {
+//     console.log("First updated: ", first);
+//   }, [first]);
+//   useEffect(() => {
+//     console.log("Second updated: ", second);
+//   }, [second]);
+//   useEffect(() => {
+//     console.log("first or second updated: ", first + second);
+//   }, [first, second]);
+//   return (
+//     <>
+//       <button onClick={() => setFirst(first + 1)}>First: {first}</button>
+//       <button onClick={() => setSecond(second + 1)}>Second: {second}</button>
+//     </>
+//   );
+// };
+
+// export default App;
+
+const App = () => {
+  const [clicks, setClicks] = useState(() => {
+    const savedClicks = window.localStorage.getItem("saved-clicks");
+    if (savedClicks !== null) {
+      return savedClicks;
+    }
+    return 0;
+  });
+  useEffect(() => {
+    window.localStorage.setItem("saved-clicks", clicks);
+  }, [clicks]);
+  return (
+    <div>
+      <button onClick={() => setClicks(clicks + 1)}>
+        You clicked {clicks} times
+      </button>
+      <button onClick={() => setClicks(0)}>Reset</button>
+    </div>
+  );
+};
+
+export default App;
